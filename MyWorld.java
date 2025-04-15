b5c6b53beebcf6fd74c1ee560c6f6574f39a1397
Daniel-hhh <wjx200811@gmail.com>
2025-04-15 08:48:27 -0400

    add character

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,11 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1);
+        
+        Leo leo = new Leo();
+        addObject(leo, 250, 250);
     }
 }
