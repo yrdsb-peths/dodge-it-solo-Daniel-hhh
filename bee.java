726fe3a2c2d7ab80d42dd6441693902d67618f1e
Daniel-hhh <wjx200811@gmail.com>
2025-04-15 09:07:26 -0400

    move bee

--- bee.java
+++ bee.java
@@ -3,6 +3,22 @@ public class bee extends Actor
 {
     public void act()
     {
-        move(-3);
+        move(-10);
+        if(getX()<=0)
+        {
+            resetbee();
+        }
     }
+public void resetbee()
+{
+    int num = Greenfoot.getRandomNumber(2);
+    if(num == 0)
+    {
+        setLocation(600,100);
+    }
+    else
+    {
+        setLocation(600,300);
+    }
+}
 }
